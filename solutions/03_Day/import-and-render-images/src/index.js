b148cd70ac897f5c7fa5a9450a3f96211f0e39cd
AlessandroChiotti <ac@tomorrowseducation.com>
2024-01-10 22:02:26 +0100

    feat (03_Day): complete exrecise 2 of day 3

--- solutions/03_Day/import-and-render-images/src/index.js
+++ solutions/03_Day/import-and-render-images/src/index.js
@@ -1,6 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import cssLogo from "./images/css_logo.png";
+import htmlLogo from "./images/html_logo.png";
+import reactLogo from "./images/react_logo.png";
+
+const header = (
+  <header>
+    <div className="header-wrapper">
+      <h1>Front End Technologies</h1>
+    </div>
+  </header>
+);
+
+const logos = (
+  <div>
+    <img src={cssLogo} alt="cssLogo" />
+    <img src={htmlLogo} alt="htmlLogo" />
+    <img src={reactLogo} alt="reactLogo" />
+  </div>
+);
+
+// JSX element, main
+const main = (
+  <main>
+    <div className="main-wrapper">{logos}</div>
+  </main>
+);
+
+// JSX element, app
+const app = (
+  <div className="app">
+    {header}
+    {main}
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(app);
