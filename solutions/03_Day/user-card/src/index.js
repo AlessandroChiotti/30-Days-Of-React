80a47c7c69fd6df3106be421b97c2676d167b230
AlessandroChiotti <ac@tomorrowseducation.com>
2024-01-16 10:34:58 +0100

    feat (03_Day): complete exercise 1 level 3

--- solutions/03_Day/user-card/src/index.js
+++ solutions/03_Day/user-card/src/index.js
@@ -1,45 +1,67 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import asabenehImage from "./images/asabeneh.jpg";
+import userProfilePicture from "./images/asabeneh.jpg";
+import checkMarkIcon from "./images/checkmark.svg";
+import clockIcon from "./images/clock-line-icon.svg";
 
-const header = (
-  <header>
-    <div className="header-wrapper">
-      <img src={asabenehImage} alt="asabeneh" />
-      <h1>Asabeneh Yetayeh</h1>
-      <h2>Senior Developer, Finland</h2>
-      <p>SKILLS</p>
-      <div className="skills">
-        <p>HTML</p>
-        <p>CSS</p>
-        <p>Sass</p>
-        <p>Js</p>
-        <p>React</p>
-        <p>Redux</p>
-        <p>Node</p>
-        <p>MongoDB</p>
-        <p>Python</p>
-        <p>Flask</p>
-        <p>Django</p>
-        <p>NumPy</p>
-        <p>Pandas</p>
-        <p>Data Analysis</p>
-        <p>MySQL</p>
-        <p>GraphQL</p>
-        <p>D3.js</p>
-        <p>Gatsby</p>
-        <p>Docker</p>
-        <p>Heroku</p>
-        <p>Git</p>
-      </div>
+const user = {
+  name: "Asabeneh Yetayeh",
+  jobTitle: "Senior Developer",
+  country: "Finland",
+  skills: [
+    "HTML",
+    "CSS",
+    "Sass",
+    "Js",
+    "React",
+    "Redux",
+    "Node",
+    "MongoDB",
+    "Python",
+    "Flask",
+    "Django",
+    "NumPy",
+    "Pandas",
+    "Data Analysis",
+    "MySQL",
+    "GraphQL",
+    "D3.js",
+    "Gatsby",
+    "Docker",
+    "Heroku",
+    "Git",
+  ],
+};
+
+const skillsFormatted = user.skills.map((skill) => (
+  <button disabled className="skillButton">
+    {skill}
+  </button>
+));
+
+const card = (
+  <main>
+    <div className="main-wrapper">
+      <img src={userProfilePicture} alt="" className="userProfilePicture" />
+      <h1>
+        {user.name} <img src={checkMarkIcon} alt="" className="checkMarkIcon" />
+      </h1>
+      <h2>
+        {user.jobTitle}, {user.country}
+      </h2>
+      <p>
+        <b>skills</b>
+      </p>
+      <ul className="skills">{skillsFormatted}</ul>
       <footer>
+        <img src={clockIcon} alt="" className="clockIcon" />
         Joined on Aug 30, 2020 <br />
       </footer>
     </div>
-  </header>
+  </main>
 );
 
-const app = <div className="app">{header}</div>;
+const app = <div className="app">{card}</div>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(app);
