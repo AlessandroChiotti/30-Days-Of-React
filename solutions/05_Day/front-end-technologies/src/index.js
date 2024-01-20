52b5304f76df7d44b7b0d0a06b2003b0ba604ad7
AlessandroChiotti <ac@tomorrowseducation.com>
2024-01-20 17:00:34 +0100

    feat (04_Day): complete exercise 1 level 2

---
+++ solutions/05_Day/front-end-technologies/src/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import cssLogo from "./images/css_logo.png";
+import htmlLogo from "./images/html_logo.png";
+import jsLogo from "./images/js_logo.png";
+import reactLogo from "./images/react_logo.png";
+
+const Header = ({ title }) => (
+  <header>
+    <div className="header-wrapper">
+      <h1>{title}</h1>
+    </div>
+  </header>
+);
+
+const LogosComponent = ({ logos }) => {
+  return (
+    <div>
+      {logos.map(({ src, alt }) => (
+        <img src={src} alt={alt} />
+      ))}
+    </div>
+  );
+};
+
+// JSX element, main
+const Main = ({ logos }) => {
+  return (
+    <main>
+      <div className="main-wrapper">
+        <LogosComponent logos={logos} />
+      </div>
+    </main>
+  );
+};
+
+// JSX element, app
+const App = () => {
+  const title = "Front End Technologies";
+
+  const logos = [
+    { src: htmlLogo, alt: "htmlLogo" },
+    { src: cssLogo, alt: "cssLogo" },
+    { src: jsLogo, alt: "jsLogo" },
+    { src: reactLogo, alt: "reactLogo" },
+  ];
+
+  return (
+    <div className="app">
+      <Header title={title} />
+      <Main logos={logos} />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
