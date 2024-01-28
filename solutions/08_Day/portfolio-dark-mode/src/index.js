6b0412f006c8d71e326027d1dd5a36fae177e29f
AlessandroChiotti <ac@tomorrowseducation.com>
2024-01-28 13:44:49 +0100

    feat (08_Day): complete exercise 1 level 2

--- solutions/08_Day/portfolio-dark-mode/src/index.js
+++ solutions/08_Day/portfolio-dark-mode/src/index.js
@@ -2,10 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import asabenehImage from "./images/asabeneh.jpg";
 
+const userCardStyle = {
+  width: "20%",
+  borderRadius: "50%",
+};
+
+const mainWrapperStyle = {
+  paddingLeft: "100px",
+  fontFamily: "sans-serif",
+  paddingTop: "20px",
+  paddingBottom: "30px",
+};
+
+const headerStyle = {
+  paddingLeft: "100px",
+  fontFamily: "sans-serif",
+  background: "#61dbfb",
+  marginBottom: "2px",
+  paddingBottom: "20px",
+  paddingTop: "20px",
+};
+
+const footerStyle = {
+  fontFamily: "sans-serif",
+  fontWeight: "300",
+  fontSize: "14px",
+  textAlign: "center",
+  background: "#61dbfb",
+  position: "absolute",
+  bottom: "0",
+  left: "0",
+  width: "100%",
+  paddingTop: "20px",
+};
+
 // User Card Component
 const UserCard = ({ user: { firstName, lastName, image } }) => (
   <div className="user-card">
-    <img src={image} alt={firstName} />
+    <img src={image} alt={firstName} style={userCardStyle} />
     <h2>
       {firstName}
       {lastName}
@@ -42,10 +76,22 @@ class Header extends React.Component {
       subtitle,
       author: { firstName, lastName },
       date,
+      styles: { backgroundColor, color },
     } = this.props.data;
 
     return (
-      <header style={this.props.styles}>
+      <header
+        style={{
+          ...headerStyle,
+          color,
+          ...{
+            background:
+              backgroundColor !== "white"
+                ? backgroundColor
+                : headerStyle.background,
+          },
+        }}
+      >
         <div className="header-wrapper">
           <h1>{welcome}</h1>
           <h2>{title}</h2>
@@ -74,11 +120,17 @@ class TechList extends React.Component {
 // Class Component
 class Main extends React.Component {
   render() {
-    const { techs, user, greetPeople, handleTime, changeBackground } =
-      this.props;
+    const {
+      techs,
+      user,
+      greetPeople,
+      handleTime,
+      changeBackground,
+      styles: { backgroundColor, color },
+    } = this.props;
     return (
-      <main>
-        <div className="main-wrapper">
+      <main style={{ background: backgroundColor, color }}>
+        <div className="main-wrapper" style={mainWrapperStyle}>
           <p>Prerequisite to get started react.js:</p>
           <ul>
             <TechList techs={techs} />
@@ -105,10 +157,25 @@ class Main extends React.Component {
 // Class component
 class Footer extends React.Component {
   render() {
+    const {
+      date,
+      styles: { backgroundColor, color },
+    } = this.props;
     return (
-      <footer>
+      <footer
+        style={{
+          ...footerStyle,
+          color,
+          ...{
+            background:
+              backgroundColor !== "white"
+                ? backgroundColor
+                : headerStyle.background,
+          },
+        }}
+      >
         <div className="footer-wrapper">
-          <p>Copyright {this.props.date.getFullYear()}</p>
+          <p>Copyright {date.getFullYear()}</p>
         </div>
       </footer>
     );
@@ -116,11 +183,23 @@ class Footer extends React.Component {
 }
 
 class App extends React.Component {
+  defaultBackground = "white";
+  darkModeBackground = "#00162A";
+  defaultColor = "black";
+  darkModeColor = "white";
+
+  defaultStyles = {
+    backgroundColor: this.defaultBackground,
+    color: this.defaultColor,
+  };
+
+  darkModeStyles = {
+    backgroundColor: this.darkModeBackground,
+    color: this.darkModeColor,
+  };
+
   state = {
-    styles: {
-      backgroundColor: "",
-      color: "",
-    },
+    styles: this.defaultStyles,
   };
   showDate = (time) => {
     const months = [
@@ -149,7 +228,21 @@ class App extends React.Component {
   greetPeople = () => {
     alert("Welcome to 30 Days Of React Challenge, 2020");
   };
-  changeBackground = () => {};
+  isDefaultStylesSet = () => {
+    return (
+      this.state.styles.backgroundColor === this.defaultBackground &&
+      this.state.styles.color === this.defaultColor
+    );
+  };
+  changeBackground = () => {
+    return this.isDefaultStylesSet()
+      ? this.setState({
+          styles: this.darkModeStyles,
+        })
+      : this.setState({
+          styles: this.defaultStyles,
+        });
+  };
   render() {
     const data = {
       welcome: "Welcome to 30 Days Of React",
@@ -160,6 +253,7 @@ class App extends React.Component {
         lastName: "Yetayeh",
       },
       date: "Oct 7, 2020",
+      styles: this.state.styles,
     };
     const techs = ["HTML", "CSS", "JavaScript"];
     // copying the author from data object to user variable using spread operator
@@ -172,11 +266,12 @@ class App extends React.Component {
         <Main
           user={user}
           techs={techs}
+          styles={data.styles}
           handleTime={this.handleTime}
           greetPeople={this.greetPeople}
           changeBackground={this.changeBackground}
         />
-        <Footer date={new Date()} />
+        <Footer date={new Date()} styles={data.styles} />
       </div>
     );
   }
